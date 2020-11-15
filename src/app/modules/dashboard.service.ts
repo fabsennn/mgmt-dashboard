1bdcc07cf845984204e089e85877a04ec99de642
Marc Horbach <marc.horbach@alumni.fh-aachen.de>
2020-11-15 16:15:16 +0100

    versuch der db anbindung

--- src/app/modules/dashboard.service.ts
+++ src/app/modules/dashboard.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {resolve} from 'url';
+import {environment} from '../../environments/environment';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +13,16 @@ export class DashboardService {
   constructor(private httpClient: HttpClient) { }
   API_KEY = 'b4456ea12bc7480baa1e90ed98f0f798';
 
+  private async request(method: string, url: string, data?: any) {
+    const result = this.httpClient.request(method, url, {
+      body: data,
+      responseType: 'json',
+      observe: 'body'
+    });
+    return new Promise((resolve, reject) => {
+      result.subscribe(resolve, reject);
+    });
+  }
   donut() {
     return [{
       name: 'offene Gespräche',
@@ -24,4 +38,9 @@ export class DashboardService {
   public getNews() {
     return this.httpClient.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
   }
+
+  public getEvents() {
+    return this.request('GET', `${environment.serverUrl}/event`);
+  }
+
 }
