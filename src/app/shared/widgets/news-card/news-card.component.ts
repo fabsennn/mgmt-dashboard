5cf7d371b65f975da671d081d9b27442d15a007d
Fabian Kirschbaum <fabian.kirschbaum@alumni.fh-aachen.de>
2020-11-16 13:57:36 +0100

    merged all Branches.

--- src/app/shared/widgets/news-card/news-card.component.ts
+++ src/app/shared/widgets/news-card/news-card.component.ts
@@ -14,7 +14,7 @@ export class NewsCardComponent implements OnInit {
   constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
-    this.dashboardService.getEvents().subscribe((data) => {
+    this.dashboardService.getNews().subscribe((data) => {
       console.log(data);
       this.articles = data['articles'];
     });
