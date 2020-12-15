1c1dbfe7baec740e87feeb734232c2abe8ecdddd
estherneuss <esther.neuss@alumni.fh-aachen.de>
2020-12-15 12:52:24 +0100

    Datum von Neuigkeitencard wird angezeigt

--- src/app/shared/widgets/news-card/news-card.component.ts
+++ src/app/shared/widgets/news-card/news-card.component.ts
@@ -8,6 +8,7 @@ import { TranslateService } from '@ngx-translate/core';
 interface News {
   neuigkeit: string;
   dringlichkeit: string;
+  datum: string;
 }
 
 @Component({
--- src/app/shared/widgets/tasks/tasks.component.ts
+++ src/app/shared/widgets/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -10,11 +11,12 @@ export class TasksComponent implements OnInit {
   news: string;
   berater: string;
   dringlichkeit: string;
+  datum = new Date().toISOString().slice(0, 10);
   constructor(private http: HttpClient) {}
   ngOnInit() {}
 
   postData() {
-     const url = `http://localhost:8080/news/${this.news}/${this.berater}/${this.dringlichkeit}`;
+     const url = `http://localhost:8080/news/${this.news}/${this.berater}/${this.dringlichkeit}/${this.datum}`;
      this.http.post(url, {
       news: this.news,
       berater: this.berater,
