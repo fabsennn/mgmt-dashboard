1b8444475370475ce11c1eca348f29dbb520044c
Marc Horbach <marc.horbach@alumni.fh-aachen.de>
2021-01-07 17:44:26 +0100

    noch ein upload hat irgendwann mal geklappt

--- src/app/shared/widgets/tasks/tasks.component.ts
+++ src/app/shared/widgets/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
@@ -11,18 +12,50 @@ export class TasksComponent implements OnInit {
   berater: string;
   dringlichkeit: string;
   datum = new Date().toISOString().slice(0, 10);
-  datei = new File([''], '');
-  constructor(private http: HttpClient) {}
-  ngOnInit() {}
+
+  datei: File | null;
+  createQPForm: FormGroup;
+
+  constructor(private http: HttpClient, private _FormBuilder: FormBuilder, ) {
+    this.datei = null;
+  }
+  ngOnInit() {
+    this.createQPForm = this._FormBuilder.group({
+      qpName: ['', [Validators.required]],
+      file: ['', [Validators.required]]
+    });
+  }
+
+  onFileChange(event) {
+    if (event.target.files && event.target.files.length) {
+      this.datei = event.target.files[0];
+    }
+  }
+
+  createQP() {
+    if (this.createQPForm.valid && this.datei && this.datei.name) {
+      const formData: FormData = new FormData();
+      formData.append('qpName', this.createQPForm.value.qpName);
+      formData.append('file', this.datei, this.datei.name);
+    }
+  }
 
   postData() {
-    let url;
-    if (this.datei.size < 1) {
+    /*let url;
+    if (this.datei.size != null) {
       url = `http://localhost:8080/news/${this.news}/${this.berater}/${this.dringlichkeit}/${this.datum}`;
     } else {
       url = `http://localhost:8080/news/${this.news}/${this.berater}/${this.dringlichkeit}/${this.datum}/${this.datei}`;
+    }*/
+    if (this.createQPForm.valid && this.datei && this.datei.name) {
+      const formData: FormData = new FormData();
+      formData.append('qpName', this.createQPForm.value.qpName);
+      formData.append('file', this.datei, this.datei.name);
+      // use the formDat here
     }
 
+
+    const url = `http://localhost:8080/news/${this.news}/${this.berater}/${this.dringlichkeit}/${this.datum}/${this.datei}`;
     this.http.post(url, {
       news: this.news,
       berater: this.berater,
